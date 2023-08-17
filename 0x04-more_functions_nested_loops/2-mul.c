595e8f25ebf617cd6573b3166e345630f90c6878
mihigohub <mihigoe1997@gmail.com>
2023-08-18 00:09:55 +0200

    C program that multiplies two integers and return the result

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - check multiplcation of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Returns the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
