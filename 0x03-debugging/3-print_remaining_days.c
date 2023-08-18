e5803b197ebedba959dacfca5de13098ef047c43
mihigohub <mihigoe1997@gmail.com>
2023-08-18 13:55:42 +0200

    Printing the remaining days of the year and check if the date does valid according to the month and year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,7 +17,7 @@ void print_remaining_days(int month, int day, int year)
 	if ((month >= 1 && month <= 12) && (day >= 1 && day <= 31))
 	{
 		if (((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) || (month == 2 && day > 29))
-			print("Invalid date: %02d/%02d/%04d\n", month, day, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 		else if (year % 4 == 0)
 		{
 			if (month >= 2 && lyear >= 61)
