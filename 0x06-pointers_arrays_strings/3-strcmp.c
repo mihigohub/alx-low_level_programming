66dddb8ea5f0b2b6cf05e7b78cf8c1ec029eebd1
mihigohub <mihigoe1997@gmail.com>
2023-08-25 01:00:14 +0200

    Function that compare two strings and prints their differences

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strcmp - function that compares two strings
  * according to their lenght
  * @s1: string one
  * @s2: string two
- *
  * Return: return > 0 if s1 > s2,and
  * return < 0 if s1 < s2 and
  * finally return 0 if both string are equal
  */
 int _strcmp(char *s1, char *s2)
 {
-	int differ;
+	int str1, str2;
 
-	for (differ = 0; s1[differ] == s2[differ]; differ++)
-	{
-		if (s1[differ] == '\0')
-			return (0);
-		return (s1[differ] - s2[differ]);
-	}
+	str1 = strlen(s1);
+	str2 = strlen(s2);
+	if (str1 == str2)
+		return (0);
+	return (str1 - str2);
 }
