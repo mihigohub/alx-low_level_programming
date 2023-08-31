292c37b0c2f2474e8b51262a46f19191a2ea2ecd
mihigohub <mihigoe1997@gmail.com>
2023-09-01 00:35:03 +0200

    Function that check if the input number is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * is_prime_number - function that check if the input number is a prime
+ * @n: input number
+ * Return: returns 1 for true otherwise 0
+ */
+int is_prime_number(int n)
+{
+	return (before_prime(n, 2));
+}
+/**
+ * before_prime - function that check the prime number
+ * @number: first integer
+ * @prime: second integer
+ * Return: returns 1 if the first integer is prime otherwise 0
+ */
+int before_prime(int number, int prime)
+{
+	if (number == 2)
+		return (1);
+	else if (number == 0 || number % prime == 0 || number == 1 || number == -1)
+		return (0);
+	else if (prime * prime > number)
+		return (1);
+	return (before_prime(number, (prime + 1)));
+}
